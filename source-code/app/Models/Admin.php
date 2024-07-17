86ba7739cea27f8c205609df230b63df67559a72
Dima228-ux <61840336+Dima228-ux@users.noreply.github.com>
2024-07-17 14:47:13 +0300

    #add tables

---
+++ source-code/app/Models/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use CrudTrait;
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
